38b42fa7ba83e099018ba39aa24a76da34b43ec1
Bhavsar Dhruv Ajit <dhruvbhavsar9898@gmail.com>
2023-11-22 16:18:25 +0530

    update in main

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion stage"
 try:
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")  
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
